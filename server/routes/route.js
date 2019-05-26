19de1effb653e6d40932c1dd9114f2d13529ec6f
Fernando <fernando.pavanello2@gmail.com>
2019-05-26 20:23:15 +0000

    Adicionada chamada da api e criado modulo events

--- server/routes/route.js
+++ server/routes/route.js
@@ -2,14 +2,21 @@ const express = require("express");
 const router = express.Router();
 
 const axios = require("axios");
-
+/*
 const api =
   "https://gateway.marvel.com:443/v1/public/events/29?ts=thesoer&apikey=001ac6c73378bbfff488a36141458af2&hash=72e5ed53d1398abb831c3ceec263f18b";
+*/
+const api = "https://jsonplaceholder.typicode.com/posts";
 
 router.get("/", (req, res) => {
-  axios.get(api).then(response => {
-    console.log(response.data.data.results[0]);
-  });
+  axios
+    .get(api)
+    .then(response => {
+      res.status(200).json(response.data);
+    })
+    .catch(error => {
+      res.status(500).send(error);
+    });
 });
 
 module.exports = router;
